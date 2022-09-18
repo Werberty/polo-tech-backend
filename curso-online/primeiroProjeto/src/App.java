f2301f3126009f783cae3b146b965a56f595afcd
Werberty <werberty.alexandre@gmail.com>
2022-09-17 22:30:01 -0300

    Variaveis

---
+++ curso-online/primeiroProjeto/src/App.java
@@ -0,0 +1,9 @@
+public class App{
+    public static void main(String[] args) throws Exception {
+
+        String nome;
+        nome = "Werberty";
+        nome = "Brasil";
+        System.out.println("Olá, " + nome + "!");
+    }
+}
