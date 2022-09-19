edfa2bddc06ed0fbd0e384627a4af666538e6409
Werberty <werberty.alexandre@gmail.com>
2022-09-19 16:08:49 -0300

    Funções

---
+++ curso-modulo-unico/src/Funcoes.java
@@ -0,0 +1,17 @@
+public class Funcoes {
+    public static void main(String[] args) {
+        // String nomeOriginal = "Let's Code";
+        // saudacao(nomeOriginal);
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
+    }
+
+    // public static void saudacao(String nomeParametro) {
+    //     System.out.println("Hello, " + nomeParametro);
+    // }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+}
